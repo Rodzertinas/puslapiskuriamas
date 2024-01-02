a74c652d53a8176ac41e0e57c9da80a2b9eec534
Rodžertinas Žemaitis <97021838+Rodzertinas@users.noreply.github.com>
2024-01-03 00:53:05 +0200

    erty

--- js/typing.js
+++ js/typing.js
@@ -5,14 +5,10 @@ var index = 0,
 
 function playAnim() {
   setTimeout(function () {
-    // set the text of typeText to a substring of the text to be typed using index.
     typeText.innerText = textToBeTyped.slice(0, index);
     if (isAdding) {
-      // adding text
       if (index > textToBeTyped.length) {
-        // no more text to add
         isAdding = false;
-        //break: wait 2s before playing again
         setTimeout(function () {
           playAnim();
         }, 2000);
@@ -20,22 +16,16 @@ function playAnim() {
 
         
       } else {
-        // increment index by 1
         index++;
       }
     } else {
-      // removing text
       if (index === 0) {
-        // no more text to remove
         isAdding = true;
       } else {
-        // decrement index by 1
         index--;
       }
     }
-    // call itself
     playAnim();
   }, 120);
 }
-// start animation
 playAnim();
